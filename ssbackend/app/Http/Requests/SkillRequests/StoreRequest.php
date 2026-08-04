2058f953de16f87d704da14a648d3a7afc08ab58
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-08-04 19:26:07 +0300

    RENAMED

---
+++ ssbackend/app/Http/Requests/SkillRequests/StoreRequest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Requests\SkillRequests;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreRequest extends FormRequest
+{
+
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'name'=>'required|max:50|min:10|string',
+            'description'=>'required|max:200|min:10|string'
+        ];
+    }
+}
