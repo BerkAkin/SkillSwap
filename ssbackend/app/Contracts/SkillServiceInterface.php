1a842ae408809af2976f9ddb31e72659d33afb7c
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-08-04 19:25:23 +0300

    FEAT: Added find definition

--- ssbackend/app/Contracts/SkillServiceInterface.php
+++ ssbackend/app/Contracts/SkillServiceInterface.php
@@ -2,13 +2,14 @@
 
 namespace App\Contracts;
 
+use App\DTOs\SkillDTOs\ShowDTO;
 use App\DTOs\SkillDTOs\StoreDTO;
 
 interface SkillServiceInterface
 {
     public function GetAll();
     public function Create(StoreDTO $DTO);
-    public function Find();
+    public function Find(ShowDTO $DTO);
     public function Update();
     public function Delete();
 }
