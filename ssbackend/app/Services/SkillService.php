556fb1215e1c2a05c14b6d9f22ec2fe4cac61867
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-08-04 20:52:23 +0300

    FEAT: Added update and destroy service methods

--- ssbackend/app/Services/SkillService.php
+++ ssbackend/app/Services/SkillService.php
@@ -3,8 +3,10 @@
 namespace App\Services;
 
 use App\Contracts\SkillServiceInterface;
+use App\DTOs\SkillDTOs\DestroyDTO;
 use App\DTOs\SkillDTOs\ShowDTO;
 use App\DTOs\SkillDTOs\StoreDTO;
+use App\DTOs\SkillDTOs\UpdateDTO;
 use App\Models\Skill;
 
 class SkillService implements SkillServiceInterface
@@ -28,7 +30,19 @@ class SkillService implements SkillServiceInterface
         return Skill::findOrFail($DTO->id);
     }
 
-    public function Update(){}
-    public function Delete(){}
+    public function Update(UpdateDTO $DTO):Skill{
+        $skill = Skill::findOrFail($DTO->id);
+        $skill->update([
+            'name'=>  $DTO->name,
+            'description' => $DTO->description,
+        ]);
+        return $skill;
+    }
+
+
+
+    public function Destroy(DestroyDTO $DTO){
+        Skill::findOrFail($DTO->id)->delete();
+    }
 
 }
