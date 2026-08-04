f75780dd8d1b1c5be9153a6464fa355c53cc7613
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-08-04 20:51:40 +0300

    FEAT: Update and destroy definitions

--- ssbackend/app/Contracts/SkillServiceInterface.php
+++ ssbackend/app/Contracts/SkillServiceInterface.php
@@ -2,14 +2,16 @@
 
 namespace App\Contracts;
 
+use App\DTOs\SkillDTOs\DestroyDTO;
 use App\DTOs\SkillDTOs\ShowDTO;
 use App\DTOs\SkillDTOs\StoreDTO;
+use App\DTOs\SkillDTOs\UpdateDTO;
 
 interface SkillServiceInterface
 {
     public function GetAll();
     public function Create(StoreDTO $DTO);
     public function Find(ShowDTO $DTO);
-    public function Update();
-    public function Delete();
+    public function Update(UpdateDTO $DTO);
+    public function Destroy(DestroyDTO $DTO);
 }
