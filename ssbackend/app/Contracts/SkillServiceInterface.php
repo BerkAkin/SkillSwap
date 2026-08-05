81e45f042353e1f35be30ce881a34096b173b306
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-08-05 15:08:45 +0300

    REFACTOR: Added return types

--- ssbackend/app/Contracts/SkillServiceInterface.php
+++ ssbackend/app/Contracts/SkillServiceInterface.php
@@ -6,12 +6,14 @@ use App\DTOs\SkillDTOs\DestroyDTO;
 use App\DTOs\SkillDTOs\ShowDTO;
 use App\DTOs\SkillDTOs\StoreDTO;
 use App\DTOs\SkillDTOs\UpdateDTO;
+use App\Models\Skill;
+use Illuminate\Database\Eloquent\Collection;
 
 interface SkillServiceInterface
 {
-    public function GetAll();
-    public function Create(StoreDTO $DTO);
-    public function Find(ShowDTO $DTO);
-    public function Update(UpdateDTO $DTO);
+    public function GetAll(): Collection;
+    public function Create(StoreDTO $DTO): Skill;
+    public function Find(ShowDTO $DTO) : ?Skill;
+    public function Update(UpdateDTO $DTO): ?Skill;
     public function Destroy(DestroyDTO $DTO);
 }
