76d0fb6f6a71f929af26f8b7a175a2af712d4e10
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-08-05 15:12:40 +0300

    FEAT: Getter resource(dto) defined for getter operations on socials

---
+++ ssbackend/app/Http/Resources/SocialResources/GetResource.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Resources\SocialResources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class GetResource extends JsonResource
+{
+
+    public function toArray(Request $request): array
+    {
+        return [
+            'type'=>$this->type,
+            'id' => $this->id,
+        ];
+    }
+}
