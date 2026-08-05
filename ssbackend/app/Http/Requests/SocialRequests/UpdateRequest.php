d2903cf821e7db801a0fbbaed9c3e70f48943ac7
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-08-05 15:12:18 +0300

    FEAT: Store request and validation defined for updating operation on socials

---
+++ ssbackend/app/Http/Requests/SocialRequests/UpdateRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests\SocialRequests;
+
+use App\Enums\SocialTypes;
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rules\Enum;
+
+class UpdateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'type'=>['required',new Enum(SocialTypes::class)],
+        ];
+    }
+}
