19f117cff7a66b0a8319cc4163709920f258a4c6
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-08-05 15:14:36 +0300

    FEAT & FIX : Added social endpoints and fixed wrong middleware definition for logout and me endpoints which required admin priviliges

--- ssbackend/routes/api.php
+++ ssbackend/routes/api.php
@@ -2,22 +2,32 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\SkillController;
+use App\Http\Controllers\SocialController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['auth:sanctum','role:admin'])->group(function(){
+
+Route::post('/register',[AuthController::class,'register']);
+Route::post('/login',[AuthController::class,'login']);
+Route::get('/skills',[SkillController::class,'index']);
+Route::get('/socials',[SocialController::class,'index']);
+
+
+Route::middleware(['auth:sanctum'])->group(function(){
     Route::post('/logout',[AuthController::class,'logout']);
     Route::get('/me',[AuthController::class,'me']);
 });
-Route::post('/register',[AuthController::class,'register']);
-Route::post('/login',[AuthController::class,'login']);
 
-
-Route::get('/skills',[SkillController::class,'index']);
-Route::get('/skills/{id}',[SkillController::class,'show']);
 
 
 Route::middleware(['auth:sanctum','role:admin'])->group(function(){
+    Route::get('/skills/{id}',[SkillController::class,'show']);
+    Route::post('/skills',[SkillController::class,'store']);
     Route::put('/skills/{id}',[SkillController::class,'update']);
     Route::delete('/skills/{id}',[SkillController::class,'destroy']);
-    Route::post('/skills',[SkillController::class,'store']);
+
+
+    Route::get('/socials/{id}',[SocialController::class,'show']);
+    Route::post('/socials',[SocialController::class,'store']);
+    Route::put('/socials/{id}',[SocialController::class,'update']);
+    Route::delete('/socials/{id}',[SocialController::class,'destroy']);
 });
