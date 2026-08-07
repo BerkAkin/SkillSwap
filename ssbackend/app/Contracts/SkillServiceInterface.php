1d8a84b90e86e54cf70b46dbd850bde385d2be7b
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-08-07 16:03:45 +0300

    REFACTOR: Applied generic repository arch

--- ssbackend/app/Contracts/SkillServiceInterface.php
+++ ssbackend/app/Contracts/SkillServiceInterface.php
@@ -2,18 +2,12 @@
 
 namespace App\Contracts;
 
-use App\DTOs\SkillDTOs\DestroyDTO;
-use App\DTOs\SkillDTOs\ShowDTO;
 use App\DTOs\SkillDTOs\StoreDTO;
 use App\DTOs\SkillDTOs\UpdateDTO;
 use App\Models\Skill;
-use Illuminate\Database\Eloquent\Collection;
 
-interface SkillServiceInterface
+interface SkillServiceInterface extends BaseServiceInterface
 {
-    public function GetAll(): Collection;
     public function Create(StoreDTO $DTO): Skill;
-    public function Find(ShowDTO $DTO) : ?Skill;
     public function Update(UpdateDTO $DTO): ?Skill;
-    public function Destroy(DestroyDTO $DTO);
 }
