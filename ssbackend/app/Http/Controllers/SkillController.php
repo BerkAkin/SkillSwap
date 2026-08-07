d8b07952385c77e0e899925f96b6e6285031fe6b
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-08-07 16:06:02 +0300

    REFACTOR: Applied generic repository based arch therefore there will be no more dtos for id required endpoints. Controller methods modified according to this decision

--- ssbackend/app/Http/Controllers/SkillController.php
+++ ssbackend/app/Http/Controllers/SkillController.php
@@ -4,11 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\JsonResponse;
 use App\Contracts\SkillServiceInterface;
-use App\DTOs\SkillDTOs\DestroyDTO;
 use App\Http\Requests\SkillRequests\StoreRequest;
 use App\Http\Resources\SkillResources\GetResource;
 use App\DTOs\SkillDTOs\StoreDTO;
-use App\DTOs\SkillDTOs\ShowDTO;
 use App\DTOs\SkillDTOs\UpdateDTO;
 use App\Http\Requests\SkillRequests\UpdateRequest;
 
@@ -33,9 +31,8 @@ class SkillController extends Controller
     }
 
 
-    public function show(int $id){
-        $dto = ShowDTO::fromRoute($id);
-        $result = $this->service->Find($dto);
+    public function show(int $id):JsonResponse{
+        $result = $this->service->Find($id);
         return response()->json([
             'message'=>'Skill pulled successfully',
             'data' => new GetResource($result),
@@ -43,7 +40,7 @@ class SkillController extends Controller
     }
 
 
-    public function update(int $id, UpdateRequest $request){
+    public function update(int $id, UpdateRequest $request):JsonResponse{
         $dto = UpdateDTO::fromArray($request->validated(),$id);
         $result = $this->service->Update($dto);
         return response()->json([
@@ -53,9 +50,8 @@ class SkillController extends Controller
     }
 
 
-    public function destroy(int $id){
-        $dto = DestroyDTO::fromRoute($id);
-        $this->service->Destroy($dto);
+    public function destroy(int $id):JsonResponse{
+        $this->service->Destroy($id);
         return response()->json([
             'message'=>'Skill deleted successfully',
         ],201);
