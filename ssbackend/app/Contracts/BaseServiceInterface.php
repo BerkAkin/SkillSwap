b2f6a7d1de5334fc40c13441ec0f564732382e50
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-08-07 16:03:06 +0300

    FEAT: Base interface for services defined to apply generic repository patternish arch for Laravel

---
+++ ssbackend/app/Contracts/BaseServiceInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Contracts;
+
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Model;
+
+interface BaseServiceInterface
+{
+    public function GetAll(): Collection;
+    public function Find(int $id): ?Model;
+    public function Destroy(int $id);
+}
