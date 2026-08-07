6a48c646cabfa7ad1f4bf600ebda94a04ee09971
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-08-07 16:08:33 +0300

    REFACTOR: Applied generic repository based arch therefore there will be no more WET methods within service

--- ssbackend/app/Services/SocialService.php
+++ ssbackend/app/Services/SocialService.php
@@ -2,31 +2,16 @@
 
 namespace App\Services;
 
-use Illuminate\Database\Eloquent\Collection;
 use App\Contracts\SocialServiceInterface;
 use App\Models\Social;
-use App\DTOs\SocialDTOs\DestroyDTO;
-use App\DTOs\SocialDTOs\ShowDTO;
 use App\DTOs\SocialDTOs\StoreDTO;
 use App\DTOs\SocialDTOs\UpdateDTO;
 
-class SocialService implements SocialServiceInterface
+class SocialService extends BaseService implements SocialServiceInterface
 {
-    public function __construct(){}
-
-    public function GetAll(): Collection{
-        return Social::all();
-    }
-
-
-    public function Find(ShowDTO $DTO): ?Social{
-        return Social::findOrFail($DTO->id);
-    }
-
 
     public function Create(StoreDTO $DTO): Social{
         $exists = Social::where('type', $DTO->type)->exists();
-
         if ($exists) {
             throw new \Exception('Social is already registered');
         }
@@ -36,13 +21,11 @@ class SocialService implements SocialServiceInterface
     }
 
     public function Update(UpdateDTO $DTO): ?Social{
-        $social = Social::findOrFail($DTO->id);
-        $social->update(['type'=>$DTO->type]);
+        $social = $this->Find($DTO->id);
+        $social->update([
+            'type'=>$DTO->type
+        ]);
         return $social;
-    }
-
-    public function Destroy(DestroyDTO $DTO){
-         Social::findOrFail($DTO->id)->delete();
     }
 
 }
