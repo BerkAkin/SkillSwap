595b9eebba80b22635a2eae607c1470f0915fe36
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-08-07 16:03:48 +0300

    REFACTOR: Applied generic repository arch

--- ssbackend/app/Contracts/SocialServiceInterface.php
+++ ssbackend/app/Contracts/SocialServiceInterface.php
@@ -2,18 +2,12 @@
 
 namespace App\Contracts;
 
-use Illuminate\Database\Eloquent\Collection;
 use App\Models\Social;
-use App\DTOs\SocialDTOs\DestroyDTO;
-use App\DTOs\SocialDTOs\ShowDTO;
 use App\DTOs\SocialDTOs\StoreDTO;
 use App\DTOs\SocialDTOs\UpdateDTO;
 
-interface SocialServiceInterface
+interface SocialServiceInterface extends BaseServiceInterface
 {
-    public function GetAll(): Collection;
-    public function Find(ShowDTO $DTO): ?Social;
     public function Create(StoreDTO $DTO): Social;
     public function Update(UpdateDTO $DTO): ?Social;
-    public function Destroy(DestroyDTO $DTO); 
 }
