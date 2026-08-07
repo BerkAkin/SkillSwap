910e1de53b6c45e63b15bb50e0ab64bf1682aea4
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-08-07 16:10:47 +0300

    FEAT. Added setting endpoints for admin to perform crud operations on it

--- ssbackend/routes/api.php
+++ ssbackend/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\SettingController;
 use App\Http\Controllers\SkillController;
 use App\Http\Controllers\SocialController;
 use Illuminate\Support\Facades\Route;
@@ -9,12 +10,15 @@ use Illuminate\Support\Facades\Route;
 Route::post('/register',[AuthController::class,'register']);
 Route::post('/login',[AuthController::class,'login']);
 Route::get('/skills',[SkillController::class,'index']);
-Route::get('/socials',[SocialController::class,'index']);
 
 
 Route::middleware(['auth:sanctum'])->group(function(){
     Route::post('/logout',[AuthController::class,'logout']);
-    Route::get('/me',[AuthController::class,'me']);
+    
+    Route::get('/me',[AuthController::class,'me']); //USER CONTROLLERA ALINACAK 
+
+    Route::get('/socials',[SocialController::class,'index']);
+    Route::get('/settings',[SettingController::class,'index']);  //USER CONTROLLERA ALINACAK me altına yeni kullanıcı bazlı yollar eklenecek
 });
 
 
@@ -30,4 +34,11 @@ Route::middleware(['auth:sanctum','role:admin'])->group(function(){
     Route::post('/socials',[SocialController::class,'store']);
     Route::put('/socials/{id}',[SocialController::class,'update']);
     Route::delete('/socials/{id}',[SocialController::class,'destroy']);
+
+    Route::get('/settings/{id}',[SettingController::class,'show']);
+    Route::post('/settings',[SettingController::class,'store']);
+    Route::put('/settings/{id}',[SettingController::class,'update']);
+    Route::delete('/settings/{id}',[SettingController::class,'destroy']);
+
+    
 });
