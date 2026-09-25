2bba1331e6ff3270c99db44c43b54c8c43063125
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-09-25 18:52:07 +0300

    REFACTOR: is now discrete and doesn't implements base interface

--- ssbackend/app/Contracts/ISkillService.php
+++ ssbackend/app/Contracts/ISkillService.php
@@ -6,8 +6,11 @@ use App\DTOs\SkillDTOs\StoreDTO;
 use App\DTOs\SkillDTOs\UpdateDTO;
 use App\Models\Skill;
 
-interface ISkillService extends IBaseService
+interface ISkillService
 {
     public function Create(StoreDTO $DTO): Skill;
     public function Update(UpdateDTO $DTO): ?Skill;
+    public function GetAll();
+    public function Find(int $id);
+    public function Destroy(int $id);
 }
