e5e45d5975ebba5180f58f10b5db7d595bcb4698
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2026-09-25 18:51:36 +0300

    REFACTOR: All skill getter method is now getting data as paginated. Thus caching system updated and added array<-->LengthAwarePaginator swap mechanism. Added cache tags logic and cache tags flush logics to invalidate caches

--- ssbackend/app/Services/SkillService.php
+++ ssbackend/app/Services/SkillService.php
@@ -6,21 +6,36 @@ use App\Contracts\ISkillService;
 use App\Models\Skill;
 use App\DTOs\SkillDTOs\StoreDTO;
 use App\DTOs\SkillDTOs\UpdateDTO;
-use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Pagination\LengthAwarePaginator;
 use Illuminate\Support\Facades\Cache;
 
-class SkillService extends BaseService implements ISkillService
+class SkillService implements ISkillService
 {
     protected string $model = Skill::class;
 
 
-    public function GetAll(): Collection
+    public function GetAll()
     {
-        $skills = Cache::remember('skills', 300, function () {
-            return $this->model::all()->toArray();
+        $page = request()->get('page', 1);
+
+        $cacheKey = "skills:page:$page";
+
+        $skills = Cache::tags(['skills'])->remember($cacheKey, 300, function () {
+            return $this->model::paginate(1)->toArray();
         });
 
-        return Skill::hydrate($skills);
+        $skills['data'] = Skill::hydrate($skills['data']);
+
+        return new LengthAwarePaginator(
+            $skills['data'],
+            $skills['total'],
+            $skills['per_page'],
+            $skills['current_page'],
+            [
+                'path' => $skills['path'],
+                'pageName' => 'page',
+            ]
+        );
     }
 
     public function Create(StoreDTO $DTO): Skill
@@ -29,7 +44,9 @@ class SkillService extends BaseService implements ISkillService
             'name' => $DTO->name,
             'description' => $DTO->description,
         ]);
-        Cache::forget('skills');
+
+        Cache::tags(['skills'])->flush();
+
         return $skill;
     }
     public function Update(UpdateDTO $DTO): ?Skill
@@ -39,16 +56,19 @@ class SkillService extends BaseService implements ISkillService
             'name' => $DTO->name,
             'description' => $DTO->description,
         ]);
-        Cache::forget('skills');
-        Cache::forget("skills:{$DTO->id}");
+
+        Cache::forget("skills:$DTO->id");
+        Cache::tags(['skills'])->flush();
+
         return $skill;
     }
 
     public function Destroy(int $id)
     {
         $this->Find($id)->delete();
-        Cache::forget('skills');
+
         Cache::forget("skills:$id");
+        Cache::tags(['skills'])->flush();
     }
 
     public function Find(int $id): ?Skill
